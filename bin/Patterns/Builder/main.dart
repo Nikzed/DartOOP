434164254477411fd8b6360a0e915ec20e0a237a
Nized <nik.shadkov@mail.ru>
2021-08-16 07:34:22 +0300

    Builder changed

--- bin/Patterns/Builder/main.dart
+++ bin/Patterns/Builder/main.dart
@@ -1,48 +1,93 @@
-void main(){
-  print('object');
-}
-
-class Car{
-
+class Car {
   bool gps;
   int seats;
-
 }
-class CarManual{}
 
-abstract class Builder{
+class Manual {
+  String gps;
+  String seats;
+
+  // нарушает ли это идеологию Билдера?
+  String getInfo() {
+    return gps + '\n' + seats ?? "info isn't specified";
+  }
+}
+
+abstract class Builder {
   void reset();
-  void setSeats();
-  void setTripComputer();
-  void setGPS();
+  void setSeats([int seats]);
+  void setGPS(bool b);
 }
 
-class CarBuilder implements Builder{
-
+class CarBuilder implements Builder {
   Car _car = Car();
 
   @override
   void reset() {
-    // TODO: implement reset
+    _car = Car();
   }
 
   @override
-  void setSeats() {
-    // TODO: implement setSeats
+  void setSeats([int seats = 4]) {
+    _car.seats = seats;
   }
 
   @override
-  void setTripComputer() {
-    // TODO: implement setTripComputer
+  void setGPS(bool b) {
+    _car.gps = b;
   }
 
-  @override
-  void setGPS() {
-    // TODO: implement setGPS
-  }
-
-  Car getResult(){
+  Car getResult() {
     return _car;
   }
-
 }
+
+class ManualBuilder implements Builder {
+  Manual _carManual = Manual();
+
+  @override
+  void reset() {
+    _carManual = Manual();
+  }
+
+  @override
+  void setGPS(bool b) {
+    _carManual.gps = b ? 'you have GPS installed' : "you don't have GPS";
+  }
+
+  @override
+  void setSeats([int seats = 4]) {
+    _carManual.seats = 'you have $seats seats';
+  }
+
+  Manual getResult() {
+    return _carManual;
+  }
+}
+
+class Director {
+  void constructSportsCar(Builder builder) {
+    builder
+      ..reset()
+      ..setSeats(2)
+      ..setGPS(true);
+  }
+}
+
+void main() {
+  Director director = Director();
+
+  CarBuilder carBuilder = CarBuilder();
+  director.constructSportsCar(carBuilder);
+  Car car = carBuilder.getResult();
+
+  ManualBuilder manualBuilder = ManualBuilder();
+  director.constructSportsCar(manualBuilder);
+
+  Manual manual = manualBuilder.getResult();
+}
+
+// фишка билдера в том, что есть возможность
+// не создавать конструктор с огромным кол-вом
+// параметров, вместо этого можно просто задать
+// автоматические базовые значения
